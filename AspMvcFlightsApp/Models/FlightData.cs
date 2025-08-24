cc72d0b49cff4186c9fbe3c3a27465adec8bbc28
MaxEfimov <efimov@mail.ru>
2025-08-24 17:48:44 +0300

    24-08-25

--- AspMvcFlightsApp/Models/FlightData.cs
+++ AspMvcFlightsApp/Models/FlightData.cs
@@ -5,9 +5,16 @@
         public int Id { get; set; }
 
         public string Name { get; set; } = null!;
+
+        public int? AirlineId { get; set; }
         public AirlineData? Airline { get; set; }
+
+        public int? DepartureId { get; set; }
         public AirportData? Departure { get; set; }
+
+        public int? ArrivalId { get; set; }
         public AirportData? Arrival { get; set; }
+
         public DateTime DateTime { get; set; }
     }
 }
